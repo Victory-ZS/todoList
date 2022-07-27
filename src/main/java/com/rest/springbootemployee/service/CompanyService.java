78ac401827cbff5cd849d323b9d0807854fcec0c
VictoryZhao <victory.zhao@oocl.com>
2022-07-27 23:55:09 +0800

    feat: CompanyServiceTest findAllCompanies

--- src/main/java/com/rest/springbootemployee/service/CompanyService.java
+++ src/main/java/com/rest/springbootemployee/service/CompanyService.java
@@ -1,4 +1,19 @@
 package com.rest.springbootemployee.service;
 
+import com.rest.springbootemployee.entity.Company;
+import com.rest.springbootemployee.repository.CompanyRepository;
+
+import java.util.List;
+
 public class CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> findAllCompanies() {
+        return companyRepository.findAllCompanies();
+    }
 }
--- src/test/java/com/rest/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/rest/springbootemployee/service/CompanyServiceTest.java
@@ -1,10 +1,19 @@
 package com.rest.springbootemployee.service;
 
+import com.rest.springbootemployee.entity.Company;
+import com.rest.springbootemployee.entity.Employee;
 import com.rest.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
 public class CompanyServiceTest {
@@ -14,6 +23,28 @@ public class CompanyServiceTest {
 
     @InjectMocks
     CompanyService companyService;
+
+    @Test
+    void should_return_all_when_find_all_given_employees() {
+        //given
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1,"Lily1",12,"male",1000));
+        employees.add(new Employee(2,"Lily2",23,"female",2000));
+        List<Company> preparedCompany = new ArrayList<>();
+        Company firstCompany = new Company(1, "spring", employees);
+        Company secondCompany = new Company(2, "summer", employees);
+        preparedCompany.add(firstCompany);
+        preparedCompany.add(secondCompany);
+        given(companyRepository.findAllCompanies()).willReturn(preparedCompany);
+
+        //when
+        List<Company>  companies = companyService.findAllCompanies();
+
+        //then
+        assertEquals(2, companies.size());
+        assertEquals(firstCompany, companies.get(0));
+
+    }
 
 
 
