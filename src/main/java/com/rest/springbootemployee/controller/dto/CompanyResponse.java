7b53edf1b1835555203182eb6d163669e5159d11
VictoryZhao <victory.zhao@oocl.com>
2022-07-31 22:56:04 +0800

    add: CompanyResponse

---
+++ src/main/java/com/rest/springbootemployee/controller/dto/CompanyResponse.java
@@ -0,0 +1,31 @@
+package com.rest.springbootemployee.controller.dto;
+
+import javax.persistence.*;
+
+public class CompanyResponse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String companyName;
+
+    public CompanyResponse(int id, String companyName) {
+        this.id = id;
+        this.companyName = companyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
